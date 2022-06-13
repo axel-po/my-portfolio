fa6f4656b5de5845eb16d93cf9888a179fc00ceb
Axel Po <axelpoin@gmail.com>
2022-06-13 20:43:17 +0200

    beta anims with gsap

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,10 +1,17 @@
-import React from "react";
-import { Title, Text } from "../Typography/Typography";
+import { useEffect, useRef } from "react";
+import { Title } from "../Typography/Typography";
+import { gsap } from "gsap";
 
 export default function Header() {
+  const titleRef = useRef();
+
+  useEffect(() => {
+    gsap.from(titleRef.current, { rotation: "+=360" });
+  }, []);
+
   return (
     <header className='py-[120px] lg:flex lg:gap-3 lg:justify-between'>
-      <Title>Portfolio</Title>
+      <Title ref={titleRef}>Portfolio</Title>
 
       <p className='text-gray text-lg lg:text-[26px] lg:leading-[40px] mt-5 max-w-[580px]'>
         Bienvenue sur mon portfolio, je suis Axel Pointud développeur web frontend spécialisé sur la librairie React JS.
