ff308ce37bfb4bec069942b3f6f29b448409ab80
Axel Po <axelpoin@gmail.com>
2022-06-18 16:06:37 +0200

    beta anim scroll

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import { Title, Text } from "../Typography/Typography";
+import { gsap } from "gsap/dist/gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { useRef, useEffect } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function About({ about }) {
+  const TL = gsap.timeline();
+  const titleRef = useRef();
+
+  useEffect(() => {
+    TL.to(titleRef.current, {
+      autoAlpha: 1,
+      y: 0,
+      duration: 0.5,
+      ScrollTrigger: {
+        trigger: titleRef.current,
+        start: "top center",
+        markers: true,
+      },
+    });
+  }, [TL]);
+
   return (
     <section className='my-[120px] lg:flex lg:justify-between'>
-      <Title>About</Title>
+      <h2 ref={titleRef} className='text-white tracking-wide text-clamp-xl translate-y-[20px] opacity-0 invisible'>
+        About
+      </h2>
       <div className='mt-5 lg:max-w-[650px]'>
         <Text>{about?.description ?? <span className='text-red-500'>Erreur lors du chargement des données</span>}</Text>
       </div>
--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -1,16 +1,56 @@
 import Project from "../Project/Project";
 import { Title } from "../Typography/Typography";
+import { gsap } from "gsap/dist/gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { useRef, useEffect } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Projects({ works }) {
+  const titleRef = useRef();
+  const projectRef = useRef([]);
+
+  const addToRefs = (el) => {
+    if (el && !projectRef.current.includes(el)) {
+      projectRef.current.push(el);
+    }
+  };
+
+  useEffect(() => {
+    gsap.to(projectRef.current, {
+      autoAlpha: 1,
+      y: 0,
+      duration: 2,
+      scrollTrigger: {
+        trigger: titleRef.current,
+        start: "top center",
+      },
+    });
+
+    gsap.to(titleRef.current, {
+      autoAlpha: 1,
+      y: 0,
+      duration: 2,
+      scrollTrigger: {
+        trigger: titleRef.current,
+        start: "top center",
+      },
+    });
+  }, []);
+
   return (
     <section>
-      <Title>Projets</Title>
+      <h2 ref={titleRef} className='text-white tracking-wide text-clamp-xl invisible opacity-0 translate-y-[50px]'>
+        Projets
+      </h2>
       {works.length == 0 ? (
         <p>Pas de projets.</p>
       ) : (
         <div className='grid md:grid-cols-2 gap-12 mt-[30px] mb-[55px]'>
           {works.map((work, id) => (
-            <Project key={id} work={work} />
+            <div key={id} ref={addToRefs} className='invisible opacity-0 translate-y-[20px]'>
+              <Project work={work} />
+            </div>
           ))}
         </div>
       )}
