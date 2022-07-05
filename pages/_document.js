f14e6e61642628dd1db60971693b4bea0e25ab3d
Axel Po <axelpoin@gmail.com>
2022-07-05 16:52:19 +0200

    git add hotjar

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,22 @@
 // eslint-disable-next-line @next/next/no-document-import-in-page
 import { Html, Head, Main, NextScript } from "next/document";
+import { hotjar } from "react-hotjar";
+
+hotjar.initialize(hjid, hjsv);
+
+// Identify the user
+hotjar.identify("USER_ID", { userProperty: 2926235 });
+
+// Add an event
+hotjar.event("button-click");
+
+// Update SPA state
+hotjar.stateChange("/");
 
 export default function Document() {
   return (
     <Html lang='fr' className='h-full'>
-      <Head />
+      <Head></Head>
       <body className='h-full'>
         <Main />
         <NextScript />
